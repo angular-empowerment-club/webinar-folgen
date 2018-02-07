ce6ddcc478d3734796ec6b669dc598a883c83755
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-02-07 23:05:36 +0100

    folge-5: move stocks related stuff into own module

--- apps/folge-5/src/app/app.module.ts
+++ apps/folge-5/src/app/app.module.ts
@@ -4,35 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { LOCALE_ID, NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { StockCardComponent } from './stock-card/stock-card.component';
-import { StockDetailsComponent } from './stock-details/stock-details.component';
-import { UpperOrLowercasePipe } from './pipes/upper-or-lowercase.pipe';
+import { StocksModule } from './stocks/stocks.module';
 
-import { StockRiskSwitcherComponent } from './stock-risk-switcher/stock-risk-switcher.component';
-import { RiskBadgeComponent } from './stock-risk-badge/stock-risk-badge.component';
-import { StockRiskFilterComponent } from './stock-risk-filter/stock-risk-filter.component';
-import { RestrictStocksPipe } from './pipes/restrict-stocks.pipe';
+import { AppComponent } from './app.component';
 
 import localeDe from '@angular/common/locales/de';
-import { StockSearchComponent } from './stock-search/stock-search.component';
-import { SearchSymbolPipe } from './pipes/search-symbol.pipe';
 registerLocaleData(localeDe);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StockCardComponent,
-    StockDetailsComponent,
-    UpperOrLowercasePipe,
-    StockRiskSwitcherComponent,
-    RiskBadgeComponent,
-    StockRiskFilterComponent,
-    StockSearchComponent,
-    RestrictStocksPipe,
-    SearchSymbolPipe
-  ],
-  imports: [BrowserModule, BrowserAnimationsModule, FormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, StocksModule],
   providers: [{ provide: LOCALE_ID, useValue: 'de' }],
   bootstrap: [AppComponent]
 })
