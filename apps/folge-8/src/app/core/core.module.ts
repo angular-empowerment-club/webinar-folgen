3e0173872deca10aaf698d609bf5dc87217d1ae8
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-03-08 18:16:23 +0100

    folge-8: add error handlers

--- apps/folge-8/src/app/core/core.module.ts
+++ apps/folge-8/src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 
 import { environment } from '../../environments/environment';
 import { AlphavantageStocks } from './alphavantage/aplhavantage.service';
 import { ALPHAVANTAGE_API_KEY } from './app-options';
 import { AttachAlphavantageApiKey } from './interceptors/attach-alphavantage-api-key.interceptor';
+import { HandleErrorsInterceptor } from './interceptors/handle-errors.interceptor';
+import { AppErrorHandler } from './app-error-handler';
 
 @NgModule({
   imports: [
@@ -12,9 +14,15 @@ import { AttachAlphavantageApiKey } from './interceptors/attach-alphavantage-api
   ],
   providers: [
     AlphavantageStocks,
+    { provide: ErrorHandler, useClass: AppErrorHandler },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AttachAlphavantageApiKey,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HandleErrorsInterceptor,
       multi: true
     },
     {
