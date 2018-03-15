df98f51959a1d5c2c59056f126a9248885b2942b
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-03-15 15:49:58 +0100

    folge-9: display modal dynamically

--- apps/folge-9/src/app/app.component.ts
+++ apps/folge-9/src/app/app.component.ts
@@ -1,7 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewContainerRef, ComponentRef } from '@angular/core';
+import { ModalComponent } from './lib/modal/modal.component';
 
 @Component({
   selector: 'aec-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: ['a { cursor: pointer; display: inline-block; text-align: center; padding: 8px; width: 100%; }']
 })
-export class AppComponent {}
+export class AppComponent {
+  private _modal: ComponentRef<ModalComponent>;
+
+  constructor(
+    private _root: ViewContainerRef,
+    private _resolver: ComponentFactoryResolver
+  ) {}
+
+  open() {
+    const modalFactory = this._resolver.resolveComponentFactory(ModalComponent);
+    this._modal = this._root.createComponent(modalFactory);
+
+    this._modal.instance.title = 'Hilfe';
+    this._modal.instance.message = 'API liefert keine Daten mehr';
+    this._modal.instance.color = 'accent';
+
+    this._modal.instance.close.subscribe(() => this._modal.destroy(), () => {}, () => console.log('COMPLETED'));
+  }
+}
--- apps/folge-9/src/app/app.module.ts
+++ apps/folge-9/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LibModule } from './lib/lib.module';
 import { StocksModule } from './stocks/stocks.module';
 
 registerLocaleData(localeDe);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StocksModule],
+  imports: [BrowserModule, LibModule, StocksModule],
   providers: [{ provide: LOCALE_ID, useValue: 'de' }],
   bootstrap: [AppComponent]
 })
