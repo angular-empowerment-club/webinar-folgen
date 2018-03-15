8e7885e48e94b1bbf2c9fc99114a2883a0f435f4
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-03-15 15:40:39 +0100

    folge-9: AppErrorHandler log the error as well

--- apps/folge-9/src/app/core/app-error-handler.ts
+++ apps/folge-9/src/app/core/app-error-handler.ts
@@ -2,6 +2,6 @@ import { ErrorHandler } from "@angular/core";
 
 export class AppErrorHandler implements ErrorHandler {
   handleError(error) {
-    console.info('AppErrorHandler: No worries I take care... :-)');
+    console.info('AppErrorHandler: No worries I take care... :-)', error);
   }
 }
